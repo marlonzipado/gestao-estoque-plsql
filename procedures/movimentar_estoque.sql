6bded898f307648a363e500332d8b26b4f25599b
root <marlon.empregos@gmail.com>
2025-06-17 05:06:15 -0300

    Criei a procedure de movimentação de estoque (agora foi)

---
+++ procedures/movimentar_estoque.sql
@@ -0,0 +1,18 @@
+CREATE PROCEDURE movimentar_estoque (
+    p_id_produto IN NUMBER,
+    p_tipo_movimentacao IN VARCHAR2,
+    p_quantidade IN NUMBER
+) AS
+BEGIN
+    INSERT INTO estoque_movimentacoes (
+    id_produto, tipo_movimentacao, quantidade
+    ) VALUES (
+        p_id_produto, UPPER(p_tipo_movimentacao), p_quantidade
+    );
+    
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK;
+        RAISE;
+END;
